9801ad1b8025057041f445fca47baf11b55c26b3
Hallqq <72313113+Halqq@users.noreply.github.com>
2022-10-26 19:25:01 -0300

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//my data types
+
+int main(){
+    //boolean
+    bool halqboolean = true;
+    bool halqbooleanFalse = false;
+
+    //char 
+    char halqChar = 'A';
+
+    //int
+    int halqint = 1;
+
+    //double
+    double halqdouble = 1.0;
+
+    //string
+    std::string halqString = "moment";
+
+    
+
+    return 0;
+}
